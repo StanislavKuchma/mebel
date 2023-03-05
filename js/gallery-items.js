47bed112cdef9f64e589ebceb4e8e438cad01df3
StanislavKuchma <kuchmastanislav73@gmail.com>
2023-03-05 13:12:19 +0200

    1commit
    1 commit

---
+++ js/gallery-items.js
@@ -0,0 +1,62 @@
+export const galleryItems = [
+  {
+    preview: "/foto/20180113_210109.jpg",
+    original: "/foto/20180113_210109.jpg",
+    description: "Hokkaido Flower",
+  },
+  {
+    preview: "/foto/20180113_210132.jpg",
+    original: "/foto/20180113_210132.jpg",
+    description: "Container Haulage Freight",
+  },
+  {
+    preview: "/foto/20190311_101259.jpg",
+    original: "/foto/20190311_101259.jpg",
+    description: "Aerial Beach View",
+  },
+  {
+    preview: "/foto/20220824_095505.jpg",
+    original: "/foto/20220824_095505.jpg",
+    description: "Flower Blooms",
+  },
+  {
+    preview: "/foto/20220824_095510.jpg",
+    original: "/foto/20220824_095510.jpg",
+    description: "Alpine Mountains",
+  },
+  {
+    preview: "/foto/20221031_083523.jpg",
+    original: "/foto/20221031_083523.jpg",
+    description: "Mountain Lake Sailing",
+  },
+  {
+    preview: "/foto/20221031_083618.jpg",
+    original: "/foto/20221031_083618.jpg",
+    description: "Alpine Spring Meadows",
+  },
+  {
+    preview: "/foto/20221031_145554.jpg",
+    original: "/foto/20221031_145554.jpg",
+    description: "Nature Landscape",
+  },
+  {
+    preview: "/foto/20221105_171819.jpg",
+    original: "/foto/20221105_171819.jpg",
+    description: "Lighthouse Coast Sea",
+  },
+  {
+    preview: "/foto/20221105_171819.jpg",
+    original: "/foto/20221105_171819.jpg",
+    description: "Lighthouse Coast Sea",
+  },
+  {
+    preview: "/foto/20221105_171819.jpg",
+    original: "/foto/20221105_171819.jpg",
+    description: "Lighthouse Coast Sea",
+  },
+  {
+    preview: "/foto/20221105_171819.jpg",
+    original: "/foto/20221105_171819.jpg",
+    description: "Lighthouse Coast Sea",
+  },
+];
